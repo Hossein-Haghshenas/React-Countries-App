8d81d7a0bd62f358b05ec0ea23a52e5b14dfa3e4
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-05-29 12:50:26 +0430

    fix something

--- src/components/Countries/Style.js
+++ src/components/Countries/Style.js
@@ -2,11 +2,16 @@ import styled from "styled-components";
 
 export const Container = styled.section`
   display: flex;
+  flex-direction: column;
+  margin-top: 4rem;
+  padding: 1rem;
+  width: calc(100%-2rem);
+  height: 100%;
+`;
+
+export const CountriesContainer = styled.section`
+  display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
   background: #fff;
-  width: calc(100%-2rem);
-  height: 100%;
-  margin-top: 4rem;
-  padding: 1rem;
 `;
