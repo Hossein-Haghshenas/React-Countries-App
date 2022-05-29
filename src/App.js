578bd9b71166a0fce50ca22ab5d0ce19a9203d4a
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-05-29 23:16:08 +0430

    Create theme

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Countries from "./components/Countries/Countries";
 import CountryDetails from "./components/CountryDetails/CountryDetails";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/ThemeMode/Style";
+import { lightTheme, darkTheme } from "./components/ThemeMode/Style";
 
 function App() {
-  const [theme, setTheme] = useState(false);
+  const [theme, setTheme] = useState(true);
 
   const darkModeHandler = (mode) => {
     setTheme(mode);
   };
 
   return (
-    <>
+    <ThemeProvider theme={!theme ? darkTheme : lightTheme}>
+      <GlobalStyles />
       <Navbar darkModeHandler={darkModeHandler} />
       <Routes>
         <Route path="/" element={<Countries />} />
         <Route path="/country/:name" element={<CountryDetails />} />
       </Routes>
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/components/ThemeMode/Style.js
+++ src/components/ThemeMode/Style.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 export const DarkMode = styled.button`
   display: flex;
@@ -11,3 +12,33 @@ export const DarkMode = styled.button`
   border-radius: 1rem;
   cursor: pointer;
 `;
+
+export const lightTheme = {
+  body: "#FFF",
+  nav: "#FFF",
+  text: "#363537",
+  toggleBorder: "#FFF",
+  background: "#363537",
+};
+export const darkTheme = {
+  body: "#363537",
+  nav: "#363537",
+  text: "#FAFAFA",
+  toggleBorder: "#6B8096",
+  background: "#999",
+  boxShadow: "0 0 7px 1px #fff",
+};
+
+export const GlobalStyles = createGlobalStyle`
+  body,
+  section,
+  nav,
+  button
+  *:before,
+  *:after
+   {
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+  }
+
+  `;
